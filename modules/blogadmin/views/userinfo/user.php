14a5f9e4459a869d61ecbe65e94034ee0e9b35e8
cfu_15811072719@163.com <cfu_15811072719@163.com>
2015-06-23 12:36:03 +0800

    新增 增加用户页面

---
+++ modules/blogadmin/views/userinfo/user.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="/public/css/base.css" rel="stylesheet" type="text/css" />
+<link href="/public/css/style.css" rel="stylesheet" type="text/css" />
+<title>管理用户</title>
+</head>
+
+<body>
+<div class="page_function">
+  <div class="info">
+    <h3><a href=ListUser.php>管理用户</a></h3>
+    <small>使用以下功能进行管理管理用户操作</small> </div>
+  <div class="exercise"> 
+   <a href="AddUser.php?enews=AddUser">增加用户</a></div>
+</div>
+<div class="page_main">  <div class="page_table table_list">
+<table width="100%" border="0" align="center" cellpadding="3" cellspacing="1" class="tableborder">
+  <tr class="header"> 
+    <td width="40" height="25"><div align="center"><a href="ListUser.php?orderby=0&mydesc=1">ID</a></div></td>
+    <td width="80" height="25"><div align="center"><a href="ListUser.php?orderby=1&mydesc=">用户名</a></div></td>
+    <td width="180"><div align="center"><a href="ListUser.php?orderby=2&mydesc=">用户组</a></div></td>
+    <td width="60"><div align="center"><a href="ListUser.php?orderby=3&mydesc=">状态</a></div></td>
+    <td width="60"><div align="center">证书</div></td>
+    <td width="60"><div align="center"><a href="ListUser.php?orderby=4&mydesc=">登陆次数</a></div></td>
+    <td><a href="ListUser.php?orderby=5&mydesc=">最后登陆</a></td>
+    <td width="110" height="25"><div align="center">操作</div></td>
+  </tr>
+    <tr> 
+    <td height="25"><div align="center"> 
+        8      </div></td>
+    <td height="25"><div align="center"> 
+        Zivz      </div></td>
+    <td> <div align="center"> 
+        管理员      </div></td>
+    <td><div align="center"> 
+        开启      </div></td>
+    <td><div align="center">--</div></td>
+    <td><div align="center">
+        27      </div></td>
+    <td> 时间：
+      2015-05-13 17:22:15&nbsp;&nbsp;&nbsp;
+      IP：
+      118.186.137.214    </td>
+    <td height="25"><div align="center">[<a href="AddUser.php?enews=EditUser&userid=8">修改</a>] 
+        [<a href="ListUser.php?enews=DelUser&userid=8" onclick="return confirm('确认要删除？');">删除</a>]</div></td>
+  </tr>
+    <tr> 
+    <td height="25"><div align="center"> 
+        7      </div></td>
+    <td height="25"><div align="center"> 
+        yanhao      </div></td>
+    <td> <div align="center"> 
+        编辑      </div></td>
+    <td><div align="center"> 
+        开启      </div></td>
+    <td><div align="center">--</div></td>
+    <td><div align="center">
+        73      </div></td>
+    <td> 时间：
+      2015-05-13 17:27:21&nbsp;&nbsp;&nbsp;
+      IP：
+      118.186.137.214    </td>
+    <td height="25"><div align="center">[<a href="AddUser.php?enews=EditUser&userid=7">修改</a>] 
+        [<a href="ListUser.php?enews=DelUser&userid=7" onclick="return confirm('确认要删除？');">删除</a>]</div></td>
+  </tr>
+    <tr> 
+    <td height="25"><div align="center"> 
+        6      </div></td>
+    <td height="25"><div align="center"> 
+        liuliya      </div></td>
+    <td> <div align="center"> 
+        编辑      </div></td>
+    <td><div align="center"> 
+        开启      </div></td>
+    <td><div align="center">--</div></td>
+    <td><div align="center">
+        87      </div></td>
+    <td> 时间：
+      2015-05-13 09:41:07&nbsp;&nbsp;&nbsp;
+      IP：
+      118.186.137.214    </td>
+    <td height="25"><div align="center">[<a href="AddUser.php?enews=EditUser&userid=6">修改</a>] 
+        [<a href="ListUser.php?enews=DelUser&userid=6" onclick="return confirm('确认要删除？');">删除</a>]</div></td>
+  </tr>
+    <tr> 
+    <td height="25"><div align="center"> 
+        5      </div></td>
+    <td height="25"><div align="center"> 
+        路由器      </div></td>
+    <td> <div align="center"> 
+        编辑      </div></td>
+    <td><div align="center"> 
+        开启      </div></td>
+    <td><div align="center">--</div></td>
+    <td><div align="center">
+        12      </div></td>
+    <td> 时间：
+      2015-04-10 18:09:28&nbsp;&nbsp;&nbsp;
+      IP：
+      211.157.176.230    </td>
+    <td height="25"><div align="center">[<a href="AddUser.php?enews=EditUser&userid=5">修改</a>] 
+        [<a href="ListUser.php?enews=DelUser&userid=5" onclick="return confirm('确认要删除？');">删除</a>]</div></td>
+  </tr>
+    <tr> 
+    <td height="25"><div align="center"> 
+        3      </div></td>
+    <td height="25"><div align="center"> 
+        3K酱      </div></td>
+    <td> <div align="center"> 
+        编辑      </div></td>
+    <td><div align="center"> 
+        开启      </div></td>
+    <td><div align="center">--</div></td>
+    <td><div align="center">
+        51      </div></td>
+    <td> 时间：
+      2015-05-11 16:53:46&nbsp;&nbsp;&nbsp;
+      IP：
+      118.186.137.214    </td>
+    <td height="25"><div align="center">[<a href="AddUser.php?enews=EditUser&userid=3">修改</a>] 
+        [<a href="ListUser.php?enews=DelUser&userid=3" onclick="return confirm('确认要删除？');">删除</a>]</div></td>
+  </tr>
+    <tr> 
+    <td height="25"><div align="center"> 
+        1      </div></td>
+    <td height="25"><div align="center"> 
+        admin      </div></td>
+    <td> <div align="center"> 
+        管理员      </div></td>
+    <td><div align="center"> 
+        开启      </div></td>
+    <td><div align="center">--</div></td>
+    <td><div align="center">
+        356      </div></td>
+    <td> 时间：
+      2015-06-23 12:29:17&nbsp;&nbsp;&nbsp;
+      IP：
+      192.168.110.218    </td>
+    <td height="25"><div align="center">[<a href="AddUser.php?enews=EditUser&userid=1">修改</a>] 
+        [<a href="ListUser.php?enews=DelUser&userid=1" onclick="return confirm('确认要删除？');">删除</a>]</div></td>
+  </tr>
+    <tr bgcolor="ffffff"> 
+    <td height="25" colspan="8"> 
+      <span class="epages"><a title="总数">&nbsp;<b>6</b> </a>&nbsp;&nbsp;</span>    </td>
+  </tr>
+</table></div></div>
+</body>
+</html>
